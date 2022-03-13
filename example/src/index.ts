6e1496644baf795eb0df4add010aad27d0ebff86
Kai <kai@dynaikon.com>
2022-03-13 17:39:19 +0000

    connection details

--- example/src/index.ts
+++ example/src/index.ts
@@ -2,7 +2,18 @@
 import { Kbot} from '../../kbot/src'
 import { DemoStrategy } from './Strategies'
 
-const bot = new Kbot()
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const binanceConnectionDetails = {
+    apiKey: process.env.API_KEY ?? "",
+    apiSecret: process.env.API_SECRET ?? "",
+    binanceDevMode: (process.env.DEV_MODE ?? "false") !== "false" // defaults to dev mode unless "false" is the value
+}
+
+const bot = new Kbot(binanceConnectionDetails)
+
 bot.addStrategy(new DemoStrategy("Demo Strategy 1"))
 
 
--- kbot/src/bot/Kbot.ts
+++ kbot/src/bot/Kbot.ts
@@ -1,16 +1,53 @@
 import { Strategy } from './strategies/Strategy'
 import { LoggerInstance } from './logging'
 
+type ConnectDetails = {
+    apiKey: string,
+    apiSecret: string,
+    binanceDevMode: boolean
+
+}
+
 export class Kbot {
     private strategies: Strategy[]
+    private apiKey: string
+    private apiSecret: string
+    private binanceDevMode: boolean
 
-    constructor() {
+    constructor(connectionDetails: ConnectDetails) {
         LoggerInstance.info("Created new instance of KBot")
+        LoggerInstance.info(connectionDetails)
         this.strategies = []
+        this.apiKey = connectionDetails.apiKey
+        this.apiSecret = connectionDetails.apiSecret
+        this.binanceDevMode = connectionDetails.binanceDevMode
+        this.checkConnectionDetailsAreSet()
+    }
+
+    private checkConnectionDetailsAreSet() {
+        if (this.apiKey.length === 0 ) {
+            LoggerInstance.error("No Binance API KEY set.")
+            process.exit()
+        }
+
+        if (this.apiKey.length === 0 ) {
+            LoggerInstance.error("No Binance API SECRET set.")
+            process.exit()
+        }
+
+        if (!this.binanceDevMode) {
+            LoggerInstance.warn("##############################")
+            LoggerInstance.warn("##############################")
+            LoggerInstance.warn("Attention: started bot in real mode!")
+            LoggerInstance.warn("##############################")
+            LoggerInstance.warn("##############################")
+        }
     }
 
     addStrategy(strategy: Strategy) {
         LoggerInstance.info(`Adding strategy: ${strategy.name}`)
         this.strategies.push(strategy)
     }
+
+
 }
