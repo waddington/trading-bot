8a834739cb725f6d68aa62d95e92072b744c4881
Kai <kai@dynaikon.com>
2022-03-13 15:52:24 +0000

    more setup

---
+++ example/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const config = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+};
+
+
+module.exports = (env, argv) => {
+
+    return config
+}
--- kbot/src/index.ts
+++ kbot/src/index.ts
@@ -1,1 +1,13 @@
-export class Kbot {}
+import { Strategy } from './bot/strategies/Strategy'
+
+export class Kbot {
+    private strategies: Strategy[]
+
+    constructor() {
+        this.strategies = []
+    }
+
+    addStrategy(strategy: Strategy) {
+        this.strategies.push(strategy)
+    }
+}
